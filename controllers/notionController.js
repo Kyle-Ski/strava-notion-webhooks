b5ee6cc8d76a13ac08662cf6e90256611b9ff077
Kyle-Ski <Kyle.Czajkowski.Ski@gmail.com>
2022-03-07 17:53:11 -0700

    webhook event now creates notion page, WIP: map all available strava fields to notion ones, create these mapping utils, work on updating and deleting things in notion upon strava events

--- controllers/notionController.js
+++ controllers/notionController.js
@@ -1,6 +1,39 @@
+require("dotenv").config();
+const { Client, LogLevel } = require("@notionhq/client");
+const res = require("express/lib/response");
+const notion = new Client({
+  auth: process.env.NOTION_KEY,
+  logLevel: LogLevel.DEBUG,
+});
+
 const getFallback = (req, res) => {
-  res.status(200).json({ message: "hello from the notion route" });
+  addItem("Testing...")
+  return res.status(200).json({ message: "hello from the notion route" });
 };
+
+async function addItem(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id: process.env.NOTION_DATABASE_ID },
+      properties: {
+        title: {
+          title: [
+            {
+              text: {
+                content: text,
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(`Error testing notion sdk: ${error.body}`);
+    return res.status(500).json({message: `Error testing notion sdk...`})
+  }
+}
 
 module.exports = {
   getFallback,
